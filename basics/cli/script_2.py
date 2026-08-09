baadf7c9fd77978f121d735e789ed6ab5634c711
m-miletic <mariomiletic22@gmail.com>
2026-08-09 19:31:39 +0200

    working with getopt

--- basics/cli/script_2.py
+++ basics/cli/script_2.py
@@ -1,40 +1,19 @@
-import sys
-import getopt
+import sys 
+import getopt 
 
 ## --- using getopt ---
 
-try:
-    opts, args = getopt.getopt(
-        sys.argv[1:],
-        "n:m:",
-        ["number1=", "number2="]
-    )
-except getopt.GetoptError as err:
-    print(f"Error: {err}")
-    sys.exit(1)
+## sys.argv[1:] -> removes the script name
+## short_options and long_options can be combined in the same call 
 
-if len(opts) < 2:
-    print("Warning: Both numbers must be provided!")
-    print("Correct usage: python script.py -n [number1] -m [number2]")
-    sys.exit(1)
+try: 
+  opts, args = getopt.getopt(sys.argv[1:], "f:o:", ["file=", "output="]) 
+except getopt.GetoptError as err: 
+  print(f"Error: {err}") 
+  sys.exit(1) 
 
-number1 = None
-number2 = None
-
-for opt, value in opts:
-    if opt in ("-n", "--number1"):
-        number1 = float(value)
-
-    elif opt in ("-m", "--number2"):
-        number2 = float(value)
-
-product = number1 * number2
-
-print(f"Number 1: {number1}")
-print(f"Number 2: {number2}")
-print(f"Product: {product}")
-
-
-## Better usage example:
-
-## backup example TODO
+for opt, arg in opts: 
+  if opt in ("-f", "--file"): 
+    print(f"Input file: {arg}") 
+  elif opt in ("-o", "--output"): 
+    print(f"Output file: {arg}") 
