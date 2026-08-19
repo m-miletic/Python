988e41c159d85c56cd39f7eb8ac5aa1b105437ee
m-miletic <mariomiletic22@gmail.com>
2026-08-19 18:57:16 +0200

    fetching data from an API

--- basics/cli/tasks/github_user_activity/githib_user_activity.py
+++ basics/cli/tasks/github_user_activity/githib_user_activity.py
@@ -1,5 +1,11 @@
 import argparse
+import requests
+from pprint import pprint
 
 parser = argparse.ArgumentParser(description="Script that can track users github activity.")
 parser.add_argument("-un", "--username", help="Users github username.", required=True)
 args = parser.parse_args()
+
+response = requests.get(f"https://api.github.com/users/{args.username}/events")
+print(response.status_code)
+pprint(response.json())
